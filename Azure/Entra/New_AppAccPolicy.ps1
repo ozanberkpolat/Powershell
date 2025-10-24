75507b73b7b29ea5193717d08dc8ce41bfe47df0
Ozan Berk Polat <126490367+ozanberkpolat@users.noreply.github.com>
2025-10-24 11:30:24 +0300

    updated

--- Azure/Entra/New_AppAccPolicy.ps1
+++ Azure/Entra/New_AppAccPolicy.ps1
@@ -1,29 +1,28 @@
 # Variables
-$AppID = "CLIENT-ID"  # Replace with your actual Application ID
-$GroupName = "APP-ACCESS-POLCY-GROUP-NAME"
-$GroupAlias = "APP-SecGrp"
-$Description = "grants access to MAILBOX-NAME"
-$Mailboxes = @("MAILBOX")  # Add more as needed
- 
+$AppID = ""  # Replace with your actual Application ID
+$GroupName = "AGRP-ALL-AppAccPolicy-" # AGRP-ALL-AppAccPolicy-APP_REGISTRATION_NAME
+$GroupAlias = "-SecGrp" # APP_REGISTRATION_NAME-SecGrp
+$Description = "grants Mail.Send access to Barge-vetting-test@company.com" # Description for the Application Access Policy
+$Mailboxes = @("Barge-vetting-test@company.com")  # List of mailboxes to include in the policy group
+
 # Connect to Exchange Online (if not already connected)
 Connect-ExchangeOnline
  
 # Step 1: Create a Mail-Enabled Security Group
-$group = New-DistributionGroup -Name $GroupName -DisplayName $GroupName -Alias $GroupAlias -Type Security -PrimarySmtpAddress "$GroupAlias@gunvorgroup.com" -Members $null
+New-DistributionGroup -Name $GroupName -DisplayName $GroupName -Alias $GroupAlias -Type Security -PrimarySmtpAddress "$GroupAlias@company.com" -Members $null
  
 # Step 2: Add Mailboxes to the group
-foreach ($mailbox in $Mailboxes) {Add-DistributionGroupMember -Identity $group.Identity -Member $mailbox}
+foreach ($mailbox in $Mailboxes) {Add-DistributionGroupMember -Identity $GroupName -Member $mailbox}
  
 # Step 3: Get the Group ID for the Application Access Policy
-$PolicyScopeGroupId = (Get-DistributionGroup $group.Identity).ExternalDirectoryObjectId
+$PolicyScopeGroupId = (Get-DistributionGroup $GroupName).ExternalDirectoryObjectId
  
 # Step 4: Create the Application Access Policy
 New-ApplicationAccessPolicy -AppId $AppID -PolicyScopeGroupId $PolicyScopeGroupId -AccessRight RestrictAccess -Description $Description
  
  
 # Step 5: TEST
-$Identity = "MAILBOX"  # Replace with the identity you want to test
+$Identity = "Barge-vetting-test@company.com"  # Replace with the identity you want to test
 $Identity2 = "ozan.polat@company.com"  # Replace with another identity to test
 Test-ApplicationAccessPolicy -Identity $Identity -AppId $AppID
 Test-ApplicationAccessPolicy -Identity $Identity2 -AppId $AppID 
-Get-ApplicationAccessPolicy
